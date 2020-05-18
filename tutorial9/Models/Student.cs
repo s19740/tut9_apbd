243e4533507053aafcd43413a644a73008732c8e
s19740 <62670024+s19740@users.noreply.github.com>
2020-05-18 23:14:58 +0200

    Add files via upload

---
+++ tutorial9/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tutorial9.Models
+{
+    public partial class Student
+    {
+        public string IndexNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int IdEnrollment { get; set; }
+        public string Password { get; set; }
+        public string Refreshtoken { get; set; }
+        public string Salt { get; set; }
+        public string Orijinalpass { get; set; }
+
+        public virtual Enrollment IdEnrollmentNavigation { get; set; }
+    }
+}
---
+++ tutorial9/Models/s19740Context.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace tutorial9.Models
+{
+    public partial class s19740Context : DbContext
+    {
+        public s19740Context()
+        {
+        }
+
+        public s19740Context(DbContextOptions<s19740Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Enrollment> Enrollment { get; set; }
+        public virtual DbSet<Student> Student { get; set; }
+        public virtual DbSet<Studies> Studies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=db-mssql;Initial Catalog=s19740;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Enrollment>(entity =>
+            {
+                entity.HasKey(e => e.IdEnrollment)
+                    .HasName("Enrollment_pk");
+
+                entity.Property(e => e.IdEnrollment).ValueGeneratedNever();
+
+                entity.Property(e => e.StartDate).HasColumnType("date");
+
+                entity.HasOne(d => d.IdStudyNavigation)
+                    .WithMany(p => p.Enrollment)
+                    .HasForeignKey(d => d.IdStudy)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Enrollment_Studies");
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasKey(e => e.IndexNumber)
+                    .HasName("Student_pk");
+
+                entity.Property(e => e.IndexNumber).HasMaxLength(100);
+
+                entity.Property(e => e.BirthDate).HasColumnType("date");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Orijinalpass)
+                    .HasColumnName("orijinalpass")
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .HasColumnName("password")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Refreshtoken)
+                    .HasColumnName("refreshtoken")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salt)
+                    .HasColumnName("salt")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdEnrollmentNavigation)
+                    .WithMany(p => p.Student)
+                    .HasForeignKey(d => d.IdEnrollment)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Student_Enrollment");
+            });
+
+            modelBuilder.Entity<Studies>(entity =>
+            {
+                entity.HasKey(e => e.IdStudy)
+                    .HasName("Studies_pk");
+
+                entity.Property(e => e.IdStudy).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
